f5515d6cd875796719411e4513c9848174f13e1c
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2021-07-22 10:00:02 +0100

    Passport JS authentification

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const { urlencoded } = require("body-parser");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+/** 
+ * mongoose-encryption
+*/
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,17 +21,33 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(urlencoded({ extended: true }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -37,36 +61,87 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-  newUser.save((err) => {
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+})
+
+app.post("/register", (req, res) => {
+
+  User.register({ username: req.body.username }, req.body.password, (err, user) => {
     if (err) {
       console.log(err);
+      res.redirect("/register");
     } else {
-      res.render("secrets");
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets")
+      })
     }
-  });
+  })
+  // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+  //   const newUser = new User({
+  //     email: req.body.username,
+  //     password: hash  //req.body.password
+  //   });
+
+  //   newUser.save((err) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.render("secrets");
+  //     }
+  //   });
+  // });
+
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({ email: username }, (err, foundUser) => {
+
+  req.login(user, (err) => {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      })
     }
   })
+
+
+
+
+  // const username = req.body.username;
+  // const password = req.body.password;
+
+  // User.findOne({ email: username }, (err, foundUser) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       bcrypt.compare(password, foundUser.password, function (err, result) {
+  //         if (result === true) {
+  //           res.render("secrets");
+  //         }
+
+  //       });
+
+
+  //     }
+  //   }
+  // })
 })
 
 app.listen(PORT, () => {
